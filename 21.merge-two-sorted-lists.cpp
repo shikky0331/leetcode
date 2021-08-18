f8e557c823b36b1d3e63a95d1acab30dc2dae110
shikky0331 <shikky0331>
2021-08-19 08:50:37 +0900

    Aug 19th, 2021

---
+++ 21.merge-two-sorted-lists.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
+// @lc code=end
